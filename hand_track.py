e657640215e53a7e8c3880348fc62b64f64689d4
Parag <paragdurafe@outlook.com>
2021-09-30 23:52:41 +0530

    Dependencies Added

--- hand_track.py
+++ hand_track.py
@@ -1,8 +1,15 @@
 import cv2
 import mediapipe as mp
-import time
+import os
+import numpy 
 
 capture=cv2.VideoCapture(1)
 
 while True:
     success,img=capture.read()
+    cv2.imshow('Counter',img)
+
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+cv2.destroyAllWindows()
